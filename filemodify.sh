0971da4c559385f4ae6a00f6d110e85614e654a5
blindlight86 <blindlight1986@gmail.com>
2021-12-09 16:35:51 +0800

    debug

--- filemodify.sh
+++ filemodify.sh
@@ -5,14 +5,20 @@ sed -i 's/--no-cache /-y /' Dockerfile
 sed -i 's/build-base/build-essential/' Dockerfile
 sed -i 's/openssl-dev/openssl libssl-dev/' Dockerfile
 sed -i '/# Install dependencies/a\RUN apt update && apt -y upgrade' Dockerfile
+sed -i '/#install user-specific packages/a\apt update' dockerStart.sh
+sed -i 's/apk add --no-cache/apt install -y/' dockerStart.sh
 
 # Add ssh support
 sed -i '/EXPOSE 5050/a\EXPOSE 22' Dockerfile
 sed -i '/# Install dependencies/i\# Install ssh' Dockerfile
 sed -i '/# Install dependencies/i\RUN apt install -y ssh' Dockerfile
-sed -i '/# Install dependencies/i\RUN sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config' Dockerfile
-# sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
-# sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sed -i '/# Install dependencies/i\RUN sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config' Dockerfile
+sed -i '/# Install dependencies/i\RUN sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config' Dockerfile
+sed -i '/# Install dependencies/i\\n' Dockerfile
 
-# sed -i '/#install user-specific packages/a\apt update' dockerStart.sh
-# sed -i 's/apk add --no-cache/apt install -y/' dockerStart.sh
+# Change entrypoint
+sed -i 's/appdaemon -c $CONF/\/usr\/sbin\/sshd -D/' dockerStart.sh
+
+if [ -n "$sshpassword" ]; then
+  echo root:$sshpassword | chpasswd
+fi
