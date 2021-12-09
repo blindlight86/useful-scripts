f8c706b3b4c400e9bda7652b8bfb72de1e4a06e9
blindlight86 <blindlight1986@gmail.com>
2021-12-10 00:00:03 +0800

    change to openssh-server

--- filemodify.sh
+++ filemodify.sh
@@ -11,7 +11,7 @@ sed -i 's/apk add --no-cache/apt install -y/' dockerStart.sh
 # Add ssh support
 sed -i '/EXPOSE 5050/a\EXPOSE 22' Dockerfile
 sed -i '/# Install dependencies/i\# Install ssh' Dockerfile
-sed -i '/# Install dependencies/i\RUN apt install -y ssh' Dockerfile
+sed -i '/# Install dependencies/i\RUN apt install -y openssh-server' Dockerfile
 sed -i '/# Install dependencies/i\RUN sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config' Dockerfile
 sed -i '/# Install dependencies/i\RUN sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config' Dockerfile
 sed -i '/# Install dependencies/i\\n' Dockerfile
